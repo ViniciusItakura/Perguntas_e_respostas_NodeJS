f121a77f77d7ad02a5d67fd174b3453034588845
ViniciusItakura <viniciusitakura1@gmail.com>
2023-10-20 22:08:18 -0300

    Nesta versão eu aprendi a usar as variaveis do EJS, manipulação com o forEach, etc

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    const nome = "Vinicius Itakura";
-    const lang = "Javascriipt";
+app.get("/:nome/:lang", (req, res) => {
+    const nome = req.params.nome;
+    const lang = req.params.lang;
+    const exibirMsg = false;
+
+    const produtos = [
+        { nome: "Doritos", preco: 314 },
+        { nome: "Coca-cola", preco: 5 },
+        { nome: "Leite", preco: 1.45 },
+        { nome: "Carne", preco: 5 },
+        { nome: "Redbull", preco: 6 },
+        { nome: "Nescau", preco: 4 }
+    ];
+
     res.render("index", {
         nome: nome,
         lang: lang,
+        empresa: "klitCode",
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
