f48dac8f6e8097f1d08f5145f626e1ec507fc687
ViniciusItakura <viniciusitakura1@gmail.com>
2023-10-20 22:28:38 -0300

    Limpado o projeto e adicionado oo bootstrap

--- index.js
+++ index.js
@@ -2,29 +2,11 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    const nome = req.params.nome;
-    const lang = req.params.lang;
-    const exibirMsg = false;
+app.get("/", (req, res) => {
 
-    const produtos = [
-        { nome: "Doritos", preco: 314 },
-        { nome: "Coca-cola", preco: 5 },
-        { nome: "Leite", preco: 1.45 },
-        { nome: "Carne", preco: 5 },
-        { nome: "Redbull", preco: 6 },
-        { nome: "Nescau", preco: 4 }
-    ];
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "klitCode",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+    res.render("index");
 });
 
 app.listen(8080, () => {
